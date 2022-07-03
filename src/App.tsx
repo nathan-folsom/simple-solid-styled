c9d72f32cf00da60c00fef02d33ede3978112342
Nathan Folsom <n8folsom@gmail.com>
2022-07-02 19:41:51 -0700

    update app component to use style prop based styling instead of class prop based

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,35 @@ import logo from './logo.svg';
 import styles from './App.module.css';
 import { cx, styled } from "./styled";
 
-const AppContainer = styled("div", styles.App);
+const AppContainer = styled("div", `text-align: center;`);
 
-const Header = styled("header", styles.header);
+const Header = styled("header", `
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`);
 
-const Logo = styled("img", styles.logo);
+const Logo = styled("img", `
+  animation: ${styles["logo-spin"]} infinite 20s linear;
+  height: 40vmin;
+  pointer-events: none;
+`);
 
-const Link = styled("a", cx(styles.link, "bg-blue"));
+const bgBlue = `background: cornflowerblue;`
 
-const Description = styled("p", "bg-blue");
+const Link = styled("a", cx(`color: white;`, bgBlue));
+
+const Description = styled("p", bgBlue);
+
+const FancyDescription = styled(Description, `
+  color: deeppink;
+  font-style: italic;
+`);
 
 const App: Component = () => {
   return (
@@ -22,6 +42,7 @@ const App: Component = () => {
         <Description>
           Edit <code>src/App.tsx</code> and save to reload.
         </Description>
+        <FancyDescription>Simple, Solid, Styled</FancyDescription>
         <Link
           href="https://github.com/solidjs/solid"
           target="_blank"
