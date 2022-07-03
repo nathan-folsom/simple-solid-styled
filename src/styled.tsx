fef0dc76f33e8117a4c5227ec2fda17f1c56a094
Nathan Folsom <n8folsom@gmail.com>
2022-07-02 19:20:19 -0700

    add overloads for styling components as well as intrinsic elements

--- src/styled.tsx
+++ src/styled.tsx
@@ -1,17 +1,29 @@
-import { Component, JSX } from "solid-js";
+import { Component, JSX, splitProps } from "solid-js";
 import { Dynamic } from "solid-js/web";
 
 export function cx(...classes: (string | null | undefined | false)[]) {
   return classes.filter((x) => !!x).join(" ");
 }
 
+type StyleAttribute = { style?: JSX.CSSProperties | string };
+
 type Stylable = {
-  [Tag in keyof JSX.IntrinsicElements as JSX.IntrinsicElements[Tag] extends { class?: string }
+  [Tag in keyof JSX.IntrinsicElements as JSX.IntrinsicElements[Tag] extends StyleAttribute
     ? Tag
     : never]: JSX.IntrinsicElements[Tag];
 };
 
-export function styled<T extends keyof Stylable>(tag: T, className: string): Component<Stylable[T]> {
-  return (props) => <Dynamic class={cx(className, props.class)} component={tag} {...props} />;
+export function styled<T extends Component<{ style?: string }>>(
+  component: T, css: string
+): T extends Component<infer P> ? Component<P> : never;
+export function styled<T extends keyof Stylable>(tag: T, css: string): Component<Stylable[T]>;
+export function styled(
+  TagOrComponent: keyof Stylable | Component<{ style?: string }>, css: string
+): Component<{ style?: string }> {
+  return props => {
+    const [styleProps, rest] = splitProps(props, ["style"]);
+    return typeof TagOrComponent === "string"
+      ? <Dynamic {...rest} style={cx(styleProps.style, css)} component={TagOrComponent}/>
+      : <TagOrComponent {...rest} style={cx(styleProps.style, css)} />;
+  };
 }
-
